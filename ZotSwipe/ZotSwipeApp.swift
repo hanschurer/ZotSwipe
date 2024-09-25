f600bbe8df776c1e6dadf54e6902317f75fe1a2b
Han Wang <hanwangx@gmail.com>
2024-09-25 08:08:31 -0700

    Initial Commit

---
+++ ZotSwipe/ZotSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZotSwipeApp.swift
+//  ZotSwipe
+//
+//  Created by Han Wang on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZotSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
